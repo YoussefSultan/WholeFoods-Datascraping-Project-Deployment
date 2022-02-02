d0dd3fbb3082f997eb7b1818bfc7bc28008cc352
Youssef Sultan <89711840+YoussefSultan@users.noreply.github.com>
2022-02-02 14:57:36 -0500

    test

--- Deployment/streamlit_app.py
+++ Deployment/streamlit_app.py
@@ -63,12 +63,12 @@ if __name__=='__main__':
   path = sorted([f for f in pathlib.Path('scraped products dump').glob("*.pkl")], key=lambda f: f.stat().st_ctime, reverse=True)
   locpath = sorted([f for f in pathlib.Path('scraped products dump/location').glob("*.pkl")], key=lambda f: f.stat().st_ctime, reverse=True)
 
-  try:     
-    with open('Deployment/'+str(path[0]), 'rb') as handle: # loads our saved .pkl back into a variable
+  try:     # Tries to open through streamlit ('Deployment/' string is required for streamlits path)
+    with open(pathlib.Path(__file__) / 'Deployment' / 'scraped products dump' / 'WF_Sales_Feb_02_2022_Newtown_Square_PA_19073.pkl', 'rb') as handle: # loads our saved .pkl back into a variable 
       df = pickle.load(handle)
-    with open('Deployment/'+str(locpath[0]), 'rb') as handle2: # loads our saved .pkl back into a variable
+    with open(pathlib.Path(__file__) / 'Deployment' / 'scraped products dump' / 'location' / 'WF_Sales_Feb_02_2022_Newtown_Square_PA_19073.pkl', 'rb') as handle2: # loads our saved .pkl back into a variable
       location = pickle.load(handle2)
-  except:
+  except: # if working on local the path is without the 'Deployment/' string
     with open(str(path[0]), 'rb') as handle: # loads our saved .pkl back into a variable
       df = pickle.load(handle)
     with open(str(locpath[0]), 'rb') as handle2: # loads our saved .pkl back into a variable
