a0566691543cdb62b76b61b550c4ee9851bcc5ef
Youssef Sultan <89711840+YoussefSultan@users.noreply.github.com>
2022-02-10 08:19:30 -0500

    adding extra libraries

--- Deployment/wholefoods_scraper.py
+++ Deployment/wholefoods_scraper.py
@@ -29,7 +29,11 @@ try:
         browser = webdriver.Chrome(path, options=options) # Chrome Driver Windows Path --if running on windows
     else:
         try:
-            os.system("chmod 755 " + str(linuxpath))
+            os.system("chmod 755 " + str(linuxpath)) # Allow permissions for chrome driver to run on linux server (Streamlit)
+            os.system('apt-get install -y libglib2.0-0=2.50.3-2 \
+    libnss3=2:3.26.2-1.1+deb9u1 \
+    libgconf-2-4=3.2.6-4+b1 \
+    libfontconfig1=2.11.0-6.7+b1')
         except Exception as e:
             print(e)
         browser = webdriver.Chrome(linuxpath, options=options) # Chrome Driver Linux Path --if running on linux (Streamlit Debian Deployment)
